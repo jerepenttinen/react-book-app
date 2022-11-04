66d373f112459fc4a859de7b25dcfbad53e4971b
Jere Penttinen <jere@disroot.org>
2022-11-04 12:29:38 +0200

    init next

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { type NextPage } from "next";
+
+const Home: NextPage = () => {
+
+  return (
+    <>
+      <main>
+        <p className="text-2xl text-primary">Hello!</p>
+      </main>
+    </>
+  );
+};
+
+export default Home;
