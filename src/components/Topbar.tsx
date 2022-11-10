021ca14c255e534d2204ff65080584140522a395
Jere Penttinen <jere.penttinen@tuni.fi>
2022-11-10 14:13:54 +0200

    Update search bar width

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -3,12 +3,12 @@ import { IoSearchOutline } from "react-icons/io5";
 
 function Topbar() {
   return (
-    <div className="flex h-16 w-full flex-row items-center justify-between bg-base-300 py-2 px-5">
-      <div className="relative flex">
+    <div className="flex h-16 w-full flex-row items-center justify-between gap-5 bg-base-300 py-2 px-5">
+      <div className="relative flex w-full min-w-min max-w-lg">
         <input
           type="text"
           placeholder="Etsi kirjoja"
-          className="input-bordered input rounded-full pl-14"
+          className="input-bordered input w-full rounded-full pl-14"
         />
         <IoSearchOutline size="24" className="absolute inset-y-3 inset-x-4" />
       </div>
