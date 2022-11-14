42932299a5f1f2824c5a2699d8a6a6ad1e0cb1cf
Jere Penttinen <jere.penttinen@tuni.fi>
2022-11-14 13:42:11 +0200

    Delete example router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,8 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { booksRouter } from "./books";
-import { exampleRouter } from "./example";
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
   books: booksRouter,
 });
