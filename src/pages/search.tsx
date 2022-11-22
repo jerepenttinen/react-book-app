b484a227bff6cc1bd2e68c203b32501a8189173c
Jere Penttinen <jere.penttinen@tuni.fi>
2022-11-22 14:16:59 +0200

    Add basic pagination to search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -2,19 +2,29 @@ import { type NextPage } from "next";
 import { useRouter } from "next/router";
 import { trpc } from "~/utils/trpc";
 import SearchResult from "~/components/SearchResult";
+import { useState } from "react";
+
+const pageLength = 20;
 
 const SearchPage: NextPage = () => {
   const router = useRouter();
   const { q } = router.query;
+  const [page, setPage] = useState(0);
 
-  const { data: booksData, isLoading } = trpc.books.search.useQuery(
+  const {
+    data: booksData,
+    isLoading,
+    isError,
+    error,
+  } = trpc.books.search.useQuery(
     {
       term: (q as string) ?? "",
-      page: 0,
-      pageLength: 20,
+      page,
+      pageLength,
     },
     {
       enabled: !!q,
+      retry: 0,
     },
   );
 
@@ -26,15 +36,47 @@ const SearchPage: NextPage = () => {
     return <h1>Ladataan...</h1>;
   }
 
+  if (isError) {
+    return <h1>{error.data?.code}</h1>;
+  }
+
+  const lastPage = !!booksData?.totalItems
+    ? Math.ceil(booksData.totalItems / pageLength)
+    : 0;
+
+  const pageNumbers: number[] = [];
+  for (let i = 0; i < lastPage; i++) {
+    pageNumbers.push(i + 1);
+  }
+
   return (
-    <>
+    <div className="flex flex-col gap-8">
       <h1>Hakutulokset: {q}</h1>
       <div className="flex flex-col gap-4">
         {booksData?.items?.map((b) => (
           <SearchResult key={"search" + b.id} book={b} />
         ))}
       </div>
-    </>
+      <div className="btn-group">
+        <button
+          type="button"
+          className="btn"
+          disabled={page === 0}
+          onClick={() => setPage((page) => page - 1)}
+        >
+          {"<"}
+        </button>
+        <div className="btn">Sivu {page + 1}</div>
+        <button
+          type="button"
+          className="btn"
+          disabled={page === lastPage}
+          onClick={() => setPage((page) => page + 1)}
+        >
+          {">"}
+        </button>
+      </div>
+    </div>
   );
 };
 
