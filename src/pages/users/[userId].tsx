53e9fdd4713c0fc1b0a5f14b6f203bc88aa67e82
SSYSS000 <jaessys@protonmail.com>
2022-11-22 14:25:00 +0200

    Add 'reading books' in profile page

--- src/pages/users/[userId].tsx
+++ src/pages/users/[userId].tsx
@@ -2,6 +2,8 @@ import { type NextPage } from "next";
 import { useRouter } from "next/router";
 import Avatar from "~/components/Avatar";
 import { trpc } from "~/utils/trpc";
+import BookCover from "~/components/BookCover";
+import Link from "next/link";
 
 const UserPage: NextPage = () => {
   const router = useRouter();
@@ -22,6 +24,16 @@ const UserPage: NextPage = () => {
       retry: 0,
     },
   );
+  
+  const { data: readingBooksData } = trpc.books.getReadingBooks.useQuery(
+  {
+    userId: userId
+  },
+  {
+    retry: 0,
+    enabled: !!userId
+  });
+
 
   if (
     typeof userId !== "string" ||
@@ -42,11 +54,52 @@ const UserPage: NextPage = () => {
   if (!userData) {
     return <>Ei löydy!</>;
   }
-
+  
   return (
     <div className="flex flex-col gap-8">
       <Avatar user={userData} size="l" />
       <h1>{userData.name}</h1>
+      <div>
+        {userData.location && <span>{userData.location}</span>}
+        {userData.emailVerified && <span>Liittyi {userData.emailVerified.toString()}</span>}
+      </div>
+      <h3>Lempikirjat</h3>
+      <h3>Kirjasto</h3>
+      <h3>Parhaillaan lukemassa</h3>
+      {readingBooksData && readingBooksData.length > 0 && <>
+        {readingBooksData.map((savedBook) => (
+          <div
+            key={savedBook.id}
+            className="flex h-min flex-row gap-4 px-4"
+          >
+            <Link
+              href={`/books/${savedBook.bookId}`}
+              className="h-24 w-16 p-0"
+            >
+              <BookCover
+                book={savedBook.book}
+                size="s"
+                key={savedBook.id + "sidecover"}
+              />
+            </Link>
+            <div className="flex w-3/5 flex-col gap-1 p-0">
+              <Link
+                href={`/books/${savedBook.bookId}`}
+                className="font-bold"
+              >
+                {savedBook.book.name}
+              </Link>
+              <span>
+                {savedBook.book.authors ?? "Tuntematon kirjoittaja"}
+              </span>
+              <span>
+                Sivulla X/Y (Z%)
+              </span>
+            </div>
+          </div>
+         ))}
+      </>}
+      <h3>Päivitykset</h3>
     </div>
   );
 };
