b70fb33ba5b57ffd2083813055f8fd6039a66f6a
Sampsa Jarvinen <Sampsa.Jarvinen@tuni.fi>
2022-11-29 13:46:24 +0200

    add review list

--- src/pages/books/[bookId].tsx
+++ src/pages/books/[bookId].tsx
@@ -12,6 +12,8 @@ import { FormEvent, useCallback, useEffect, useState } from "react";
 import { Popover, RadioGroup } from "@headlessui/react";
 import BookCover from "~/components/BookCover";
 
+import Avatar from "~/components/Avatar";
+
 interface ReviewSectionProps {
   bookId: string;
 }
@@ -126,7 +128,7 @@ function ReviewSection(this: any, props: ReviewSectionProps) {
       <div className="divider"></div>
       <span className="font-bold">Kirjan arvostelut</span>
       {reviewData?.map((review) => (
-        <div key={review.id}>{review.content} <BookScore bookId={props.bookId} /></div>
+        <div key={review.id}>{review.content} <ReviewScore reviewScore={review.score}/> By: {review.user.name} <Avatar user={review.user} size="s" /></div>
       ))}
     </section>
   );
@@ -293,6 +295,41 @@ function BookScore({ bookId }: { bookId: string }) {
         ))}
       </div>
       <h1 className="my-0" title={`${starData?._count.score ?? 0} arvostelua`}>
+        {score.toFixed(2)}
+      </h1>
+    </div>
+  );
+}
+
+function ReviewScore({ reviewScore }: { reviewScore: number }) {
+  const score = reviewScore / 2;
+  const starPercentages = [0, 0, 0, 0, 0];
+
+  for (let i = 0; i < score; i++) {
+    starPercentages[i] = 1;
+  }
+
+  if (score < 5) {
+    // Set last star to the fraction of the score
+    starPercentages[Math.floor(score)] = score % 1;
+  }
+
+  return (
+    <div className="my-0 inline-flex gap-2">
+      <div className="inline-flex gap-px">
+        {starPercentages.map((perc, i) => (
+          <div key={i + "star"} className="relative h-10 w-10">
+            <div
+              style={{ width: `${2.5 * perc}rem` }}
+              className="absolute h-10 overflow-hidden"
+            >
+              <div className="mask mask-star-2 h-10 w-10 bg-secondary"></div>
+            </div>
+            <div className="mask mask-star-2 absolute h-10 w-10 bg-secondary/20"></div>
+          </div>
+        ))}
+      </div>
+      <h1 className="my-0">
         {score.toFixed(2)}
       </h1>
     </div>
